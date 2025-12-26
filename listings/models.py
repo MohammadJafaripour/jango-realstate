14f46abf862024efd58cb174eae5cd74e8a6b36c
MohammadJafaripour <mjafaripoursws@gmail.com>
2025-12-26 03:33:24 +0330

    realtors models added

--- listings/models.py
+++ listings/models.py
@@ -13,8 +13,8 @@ class Listing(models.Model):
     price = models.IntegerField()
     bedrooms = models.IntegerField()
     bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
-    garage = models.IntegerField(defualt=0)
-    sqft = models.IntegerField(defualt=0)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField(default=0)
     lotsize = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
@@ -23,8 +23,8 @@ class Listing(models.Model):
     photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    is_published = models.BooleanField(defualt=True)
-    list_date = models.DateTimeField(defualt=datetime.now, blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.title
 
