f38e3b89e6eef1a69343a44b6cab6c0027ef7735
MohammadJafaripour <mjafaripoursws@gmail.com>
2025-12-29 11:36:43 +0330

    fix smtp

--- btre/settings.py
+++ btre/settings.py
@@ -151,5 +151,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'm.jafaripoursws@gmail.com'
-EMAIL_HOST_PASSWORD = '#ihG4ever'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
