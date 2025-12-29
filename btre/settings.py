818a7a94ac51acf260d43570b3fc6a97603d865f
MohammadJafaripour <mjafaripoursws@gmail.com>
2025-12-29 10:49:47 +0330

    final version with added dashboard functinality

--- btre/settings.py
+++ btre/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+
 ]
 
 MIDDLEWARE = [
@@ -143,3 +145,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# Email
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'm.jafaripoursws@gmail.com'
+EMAIL_HOST_PASSWORD = '#ihG4ever'
+EMAIL_USE_TLS = True
